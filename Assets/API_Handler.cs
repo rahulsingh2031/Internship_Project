0f3c230cbac7da2a098be83ecafb88676fddb7b6
rahulsingh2031 <rahulsingh2031965@GMAIL.COM>
2023-08-04 09:19:50 +0530

    first commit

---
+++ Assets/API_Handler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+public class API_Handler : MonoBehaviour
+{
+    public static API_Handler Singleton;
+    [SerializeField]
+    private string url = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=client_data";
+
+    public Root RootData { get; private set; }
+
+    private void Awake()
+    {
+        if (Singleton == null)
+        {
+            Singleton = this;
+        }
+        else if (Singleton != this || Singleton != this)
+        {
+            Destroy(this);
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(GetJsonObject());
+    }
+    private IEnumerator GetJsonObject()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
+            }
+
+            var api_data_text = webRequest.downloadHandler.text;
+            RootData = JsonConvert.DeserializeObject<Root>(api_data_text);
+        }
+    }
+}
+//Json equivalent to C# Object
+[System.Serializable]
+public class Root
+{
+    public List<Client> clients = new();
+    public Dictionary<string, UserData> data = new();
+    public string label;
+}
+[System.Serializable]
+public class Client
+{
+    public bool isManager;
+    public int id;
+    public string label;
+}
+
+[System.Serializable]
+public class UserData
+{
+#nullable enable
+    public string? address;
+    public string? name = null;
+    public int? points;
+}
